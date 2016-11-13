1abef10b1d755e29cdeeef766d2b233d482d1552
susharp <glmail5051@qq.com>
2016-11-13 14:48:21 +0800

    decomment some code

--- virtualenv_automation.sh
+++ virtualenv_automation.sh
@@ -3,12 +3,12 @@ alias virtualenv="/usr/local/Cellar/python/2.7.12/Frameworks/Python.framework/Ve
 alias veact="source venv/bin/activate"
 function veinit(){
     projectname=$1
-    if [ $# -eq 0 -o $# -gt 1 ];then
-        echo -e "The args of project name you passed can not be 0 or greater than 1!\n"
-        return 1
-    fi
-    mkdir $projectname
-    cd $projectname
+    #if [ $# -eq 0 -o $# -gt 1 ];then
+    #    echo -e "The args of project name you passed can not be 0 or greater than 1!\n"
+    #    return 1
+    #fi
+    #mkdir $projectname
+    #cd $projectname
     virtualenv venv
     if [ $? -ne 0 ];then
         echo -e "Last comand failed,eixt!"
