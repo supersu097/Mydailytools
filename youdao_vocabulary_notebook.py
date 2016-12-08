8f4ed6d5aa2d85c611380733ecfd9e7d490f2b29
susharp <glmail5051@qq.com>
2016-12-08 18:54:03 +0800

    Autofix PEP8 errors by using the vim plugin of python-mode

--- youdao_vocabulary_notebook.py
+++ youdao_vocabulary_notebook.py
@@ -9,14 +9,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='A little tool to help me extract the words list in a xml'
         'format file which exported from youdao vocabulary notebook,notice'
-        'that the file itself must be with the xml format...') 
+        'that the file itself must be with the xml format...')
     parser.add_argument(
         'filename',
         help='The file you wanna process.',
         type=file)
     args = parser.parse_args()
-    soup=BeautifulSoup(open(args.filename.name),'html5lib')
-    with open('words.txt','w') as w:
+    soup = BeautifulSoup(open(args.filename.name), 'html5lib')
+    with open('words.txt', 'w') as w:
         for j in soup.find_all('word'):
             w.write(j.get_text() + '\n')
-
