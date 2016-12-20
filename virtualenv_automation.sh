d11cb449679e2dc07342457c90c73ab5db9b0938
susharp <glmail5051@qq.com>
2016-12-21 00:28:57 +0800

    implement the functionality of specifying which py interpreter I wanna use

--- virtualenv_automation.sh
+++ virtualenv_automation.sh
@@ -1,17 +1,14 @@
 # Created by sharp.gan at 2016-10-11
-#alias virtualenv="/usr/local/Cellar/python/2.7.12/Frameworks/Python.framework/Versions/2.7/bin/virtualenv"
 alias veact="source .venv/bin/activate"
 function veinit(){
-    projectname=$1
-    #if [ $# -eq 0 -o $# -gt 1 ];then
-    #    echo -e "The args of project name you passed can not be 0 or greater than 1!\n"
-    #    return 1
-    #fi
-    #mkdir $projectname
-    #cd $projectname
-    virtualenv -p python2 .venv
+    pyversion=$1
+    if [ $# -eq 0 ];then
+        virtualenv -p python2 .venv
+    else
+        virtualenv -p python$pyversion .venv
+    fi
     if [ $? -ne 0 ];then
-        echo -e "Last comand failed,exit!"
+        echo -e "Creating py virtual env failed,exit!"
         return 1
     fi
     veact
