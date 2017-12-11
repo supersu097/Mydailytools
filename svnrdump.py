45312936eb2d72898eea7210927d816f91d6f835
sharp <sharp.gan1993@gmail.com>
2017-12-11 15:27:32 +0800

    add new script

---
+++ svnrdump.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# coding=utf-8
+
+import os
+import subprocess
+
+
+def cmd_generator(option, path):
+    return "svnrdump dump svn://106.14.248.127/projects/ {op} > {pa}/svn_backup.dump".format(op=option, pa=path)
+
+
+DUMPED_PATH = "/home/internal-server/backup"
+if not os.path.isfile(DUMPED_PATH + "svn_backup.dump"):
+    subprocess.check_call(cmd_generator('', DUMPED_PATH))
+else:
+    subprocess.check_call(cmd_generator('--incremental', DUMPED_PATH))
